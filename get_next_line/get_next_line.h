5211214c224f526588d29d7170e81a196d238efa
Diogo Ferreira <155843454+DadiaZ24@users.noreply.github.com>
2024-03-30 23:28:51 +0000

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddias-fe <ddias-fe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 23:20:24 by ddias-fe          #+#    #+#             */
+/*   Updated: 2024/02/09 23:20:24 by ddias-fe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+//# define BUFFER_SIZE 1
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+char	*ft_strchr(char *str, char c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strndup(char *s, int len);
+char	*free_data(char *stash, char *buffer);
+char	*read_from_file(int fd, char *stash);
+char	*leftovers(char *stash);
+char	*get_next_line(int fd);
+
+int		ft_strclen(char *str, char c);
+
+#endif
